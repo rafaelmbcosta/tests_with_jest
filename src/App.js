06b8ca099b36a142740aabf6c5a1979390234c62
Rafael Costa <rafaelmbcosta@gmail.com>
2020-08-24 11:47:42 -0300

    Congrats component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
-import CountComponentFunction from './components/CountComponentFunction';
-import CountComponentClass from './components/CountComponentClass';
+// import CountComponentFunction from './components/CountComponentFunction';
+// import CountComponentClass from './components/CountComponentClass';
+import Congrats from './components/pt2/Congrats';
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello Jest/Enzyme </h1>
+      {/* <h1> Hello Jest/Enzyme </h1>
       <hr/>
       <CountComponentFunction />
       <hr/>
-      <CountComponentClass />
+      <CountComponentClass /> */}
+
+      <Congrats />
     </div>
   );
 }
---
+++ src/test/testUtils.js
@@ -0,0 +1,11 @@
+import { ShallowWrapper } from "enzyme"
+
+/**
+ * return node(s)
+ * @param {ShallowWrapper} wrapper
+ * @param {string} val
+ * @returns {ShallowWrapper}
+ */
+export const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+}
